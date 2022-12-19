3140a4f7c09de8e3501cba73f39fc46a86528018
frdnhmrf <frdnhmrf@gmail.com>
2022-12-19 23:08:53 +0700

    first commit

---
+++ intro_flutter_playground/lib/pages/animated_container_page.dart
@@ -0,0 +1,63 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class AnimatedContainerPage extends StatefulWidget {
+  @override
+  _AnimatedContainerPageState createState() => _AnimatedContainerPageState();
+}
+
+class _AnimatedContainerPageState extends State<AnimatedContainerPage> {
+  double _width = 200;
+  double _height = 200;
+  Color _color = Colors.red;
+  BorderRadiusGeometry _borderRadius = BorderRadius.circular(16);
+
+  final random = Random();
+
+  void _actionButton() {
+    setState(() {
+      _width = random.nextInt(300).toDouble();
+      _height = random.nextInt(300).toDouble();
+
+      _color = Color.fromRGBO(
+        random.nextInt(256),
+        random.nextInt(256),
+        random.nextInt(256),
+        1,
+      );
+
+      _borderRadius = BorderRadius.circular(random.nextInt(100).toDouble());
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('AnimatedContainer'),
+      ),
+      body: Center(
+        child: AnimatedContainer(
+          // Use the properties stored in the State class.
+          width: _width,
+          height: _height,
+          duration: Duration(
+            seconds: 1,
+          ),
+          decoration: BoxDecoration(
+            color: _color,
+            borderRadius: _borderRadius,
+          ),
+          curve: Curves.fastOutSlowIn,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _actionButton,
+        child: Icon(
+          Icons.play_arrow,
+        ),
+      ),
+    );
+  }
+}
